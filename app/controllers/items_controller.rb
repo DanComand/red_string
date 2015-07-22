f9c7531a9d20721df4fb8236b501c781913cb3e0
Dan Comand <dcomand@gmail.com>
2015-07-22 00:04:50 -0400

    Adding formatting to index page to show all items that have been lent out.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,49 +1,49 @@
 class ItemsController < ApplicationController
 
   def index
-    @items = Items.all
+    @items = Item.all
   end
 
   def show
-    @item = Items.find(params[:id])
+    @item = Item.find(params[:id])
   end
 
   def new
-    @item = Items.new
+    @item = Item.new
   end
 
   def edit
-    @item = Items.find(params[:id])
+    @item = Item.find(params[:id])
   end
 
   def create
-    @item = Items.new(items_params)
+  	@item = Item.new(item_params)
 
-    if @item.save
-      redirect_to items_url
-    else
-      render :new
-    end
+  	if @item.save
+  		redirect_to items_url
+  	else
+  		render :new
+  	end
   end
 
   def update
-    @item = Items.find(params[:id])
+    @item = Item.find(params[:id])
 
-    if @item.update_attributes(items_params)
-      redirect_to items_path(@item)
+    if @item.update_attributes(item_params)
+      redirect_to item_path(@item)
     else
       render :edit
     end
   end
 
   def destroy
-    @item = Items.find(params[:id])
+    @item = Item.find(params[:id])
     @item.destroy
     redirect_to items_path
   end
 
   private
-  def items_params
-    params.require(:items).permit(:name, :lent_to, :date_lent, :date_due, :phone, :email)
+  def item_params
+    params.require(:item).permit(:name, :lent_to, :date_lent, :date_due, :phone, :email)
   end
 end
